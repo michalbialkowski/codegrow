b2a5ac3901ef9b5ccc7d168d49def99d633096e7
Michał Białkowski <michal@bialkowski.net.pl>
2022-10-06 23:42:16 +0200

    Update

--- assets/js/utilities/scroll.js
+++
@@ -1,7 +0,0 @@
-$('.button--scroll').on("click", function(e) {
-	e.preventDefault();
-	var dest = $(this).attr("href");
-	$("html, body").animate({
-		'scrollTop':   $(dest).offset().top
-	}, 2000);
-});
